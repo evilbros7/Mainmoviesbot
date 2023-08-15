e5a00b4e5abef56b1dc1e552f2fb4952f2954d4d
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-08-16 02:29:21 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -166,8 +166,7 @@ async def pm_text(bot, message):
     await bot.send_message(
         chat_id=LOG_CHANNEL_PM,
         text=f"<b>#PM_MSG\n\nName: {user}\n\nID: {user_id}\n\nMessage: {content}</b>",
-        reply_markup=keyboard,      
-        quote=quote,
+        reply_markup=keyboard,
     )
 
 @Client.on_callback_query(filters.regex(r"^next"))
