c2cd7fe05dafe56e584b9348351e298cd745ca8e
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-08-15 23:34:16 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -340,6 +340,7 @@ async def advantage_spoll_choker(bot, query):
 
 @Client.on_callback_query()
 async def cb_handler(client: Client, query: CallbackQuery):
+    is_admin = query.from_user.id in ADMINS
     if query.data == "close_data":
         await query.message.delete()
     elif query.data == "delallconfirm":
